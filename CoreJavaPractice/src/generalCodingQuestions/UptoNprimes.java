2377d6f865873998269ec3ad80ee85848b7cd668
Rahul Pawar <pawarrahul699@gmail.com>
2021-07-08 09:52:43 +0530

    add code of prime numbers less than or equal to N

--- CoreJavaPractice/src/generalCodingQuestions/UptoNprimes.java
+++ CoreJavaPractice/src/generalCodingQuestions/UptoNprimes.java
@@ -3,7 +3,25 @@ package generalCodingQuestions;
 public class UptoNprimes {
 
 	public static void main(String[] args) {
-		
+
+		int target_no = 7;
+
+		for (int i = 2; i <= target_no; i++) {
+
+			int count = 0;
+			for (int j = 2; j * j <= i; j++) {
+				if (i % j == 0) {
+					count++;
+					break;
+				}
+			}
+			
+			if(count == 0) {
+				System.out.println(i);
+			}
+			
+
+		}
 
 	}
 
