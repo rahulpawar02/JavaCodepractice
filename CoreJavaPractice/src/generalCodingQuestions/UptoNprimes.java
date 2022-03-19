c41f1db640dd802539056c395ec2dc3f5c47ef4c
RahulPawar <pawarrahul699@gmail.com>
2022-03-19 16:30:57 +0530

    add normal changes

--- CoreJavaPractice/src/generalCodingQuestions/UptoNprimes.java
+++ CoreJavaPractice/src/generalCodingQuestions/UptoNprimes.java
@@ -4,6 +4,7 @@ public class UptoNprimes {
 
 	public static void main(String[] args) {
 
+		//remember: 2 is always prime no  & prime number is only divide by itself, if divide by any number then this is not prime number.
 		int target_no = 7;
 
 		for (int i = 2; i <= target_no; i++) {
