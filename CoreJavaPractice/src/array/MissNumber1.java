6edd8c3344670c878924c58a440f615f4cb51adc
Rahul Pawar <87107398+rahulpawar02@users.noreply.github.com>
2022-08-25 11:15:47 +0530

    modify MissNumber1solution with for loop

--- CoreJavaPractice/src/array/MissNumber1.java
+++ CoreJavaPractice/src/array/MissNumber1.java
@@ -1,31 +1,50 @@
 package array;
 
 public class MissNumber1 {
-
+	
 	public static int missingNumber(int[] array, int n) {
 		
-		int search = 1;
-		while(true) {
+		for(int i=1; i<=n+1; i++) {
 			
-			int i=0;
-			for(i=0;i<n;i++) {
-				if(search == array[i]) {
+			int j=0;
+			for(j=0; j<n; j++) {
+				if(i == array[j]) {
 					break;
 				}
 			}
-			
-			if(i==n) {
-				return search;
+			if(j == n) { // iterate whole array but number is not match in array element then return this number.
+				return i;
 			}
-			search++;
 		}
+		return -1;
 	}
 	
 	public static void main(String[] args) {
-		int[] array = {1,2,3,4,5};
+		int[] array = {1,2,3,5};
 		int missNum = missingNumber(array, array.length);
-		System.out.println(missNum);
+		System.out.println("Missing numbe is:" + missNum);
 
 	}
 
 }
+
+//output: Missing numbe is: 4
+
+//Using while loop:
+// 	public static int missingNumber(int[] array, int n) {		
+// 		int search = 1;
+// 		while(true) {
+			
+// 			int i=0;
+// 			for(i=0;i<n;i++) {
+// 				if(search == array[i]) {
+// 					break;
+// 				}
+// 			}
+			
+// 			if(i==n) {
+// 				return search;
+// 			}
+// 			search++;
+// 		}
+// 	}
