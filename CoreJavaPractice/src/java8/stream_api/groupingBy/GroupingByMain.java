4579478d78c70df8fd896d4aa5d0c748144e0b16
Rahul Pawar <pawarrahul699@gmail.com>
2022-12-03 13:04:26 +0530

    add the code where used java 8 groupingBy() Collectors method.

---
+++ CoreJavaPractice/src/java8/stream_api/groupingBy/GroupingByMain.java
@@ -0,0 +1,26 @@
+package java8.stream_api.groupingBy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class GroupingByMain {
+	
+	public static void  main (String args[]) {
+		
+		List<Student> studentList = new ArrayList<>();
+		studentList.add(new Student(1L,"mark", 10));
+		studentList.add(new Student(2L,"alex", 23));
+		studentList.add(new Student(3L,"Johnson",23));
+		
+		//groupingBy
+		
+		Map<Integer, List<Student>> studentAgeGrouping= studentList.stream().collect(Collectors.groupingBy(student->student.getAge()));
+		
+		//age wise create the group according to studentList.
+		System.out.println(studentAgeGrouping);	
+		
+	}
+
+}
