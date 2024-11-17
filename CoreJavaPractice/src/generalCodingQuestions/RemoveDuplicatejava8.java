b5af9bd29313cb8fada232138b4ebc31404d2ce9
Rahul Pawar <pawarrahul699@gmail.com>
2024-11-18 00:07:43 +0530

    remove duplicate java 8

--- CoreJavaPractice/src/generalCodingQuestions/RemoveDuplicatejava8.java
+++ CoreJavaPractice/src/generalCodingQuestions/RemoveDuplicatejava8.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class RemoveDuplicatejava8 {
 
@@ -12,7 +13,17 @@ public class RemoveDuplicatejava8 {
 		List<Integer> list= Arrays.asList(10,20,20,30,40,50,10,60);
 		Set<Integer> set = new HashSet<Integer>();
 		
-		 list.stream().filter(e -> set.add(e)).forEach(e->System.out.println(e));
+//	   list.stream().filter(e -> set.add(e)).forEach(e->System.out.println(e));
+		
+//		Using distinct()
+		List<Integer> uniqueList = list.stream().distinct().collect(Collectors.toList());
+		System.out.println("list" + uniqueList);
+		
+//		using removeIf() collection method.
+		System.out.println("org list" + list); 
+		list.removeIf(n -> !set.add(n));
+		System.out.println("list 2" + list);
+		 
 		 
 	}
 }
