92c12ca140c9596fd4c015efd89f1aad4f99536b
Rahul Pawar <pawarrahul699@gmail.com>
2024-11-24 01:11:05 +0530

    removed the unwanted and debugging code and log

--- CoreJavaPractice/src/threading/OddEvenThread.java
+++ CoreJavaPractice/src/threading/OddEvenThread.java
@@ -1,9 +1,10 @@
 package threading;
+//Write a program using two threads print even and odd number simultaneously
 
-public class OddEvenThread  {
+public class OddEvenThread {
 
 	public static void main(String[] args) {
-		
+
 		NumberPrinter nmObj = new NumberPrinter();
 		int max = 10;
 
@@ -15,68 +16,50 @@ public class OddEvenThread  {
 
 }
 
+class NumberPrinter {
 
-class NumberPrinter {	
-	
 	private boolean isOdd = true;
 	
 	synchronized void printOdd(int number) {
 		while (!isOdd) {
-			try {
-				wait();
-			} catch (InterruptedException e) {
-				Thread.currentThread().interrupt();
-			}
-		}	
-		System.out.println("odd task..." + Thread.currentThread().getName());
-		System.out.println("odd: "+number);
+			waitSafely();
+		}
+		System.out.println("odd: " + number);
 		isOdd = false; // Set flag to false to indicate it's even thread's turn
 		notify(); // Notify the even thread to proceed
-        try {
-            Thread.sleep(4000); // Sleep to observe the behavior
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
 	}
-	
-	
+
 	synchronized void printEven(int number) {
-		while(isOdd) {
-			try {
-				wait();
-			} catch (InterruptedException e) {
-				Thread.currentThread().interrupt();
-			}
+		while (isOdd) {
+			waitSafely();
 		}
-		System.out.println("even task..."+ Thread.currentThread().getName());
-		System.out.println("even: " + number);	
+		System.out.println("even: " + number);
 		isOdd = true; // Set flag to true to indicate it's odd thread's turn
-		notify();// Notify the odd thread to proceed	
-		
-        try {
-            Thread.sleep(4000); // Sleep to observe the behavior
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
+		notify();// Notify the odd thread to proceed
+	}
+
+	private void waitSafely() {
+		try {
+			wait();
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		}
 	}
 }
 
-
 class OddTask implements Runnable {
-	
+
 	private final NumberPrinter numberPrinter;
 	private final int max;
-	
+
 	OddTask(NumberPrinter numberPrinter, int max) {
 		this.numberPrinter = numberPrinter;
 		this.max = max;
 	}
-	
 
 	@Override
 	public void run() {
-		System.out.println("odd task...Thread started with Name is: " + Thread.currentThread().getName());
-		for(int i = 1; i < max; i+=2) {
+		for (int i = 1; i < max; i += 2) {
 			numberPrinter.printOdd(i);
 		}
 
@@ -87,18 +70,17 @@ class EvenTask implements Runnable {
 
 	private final NumberPrinter numberPrinter;
 	private final int max;
-	
+
 	EvenTask(NumberPrinter numberPrinter, int max) {
 		this.numberPrinter = numberPrinter;
 		this.max = max;
 	}
+
 	@Override
 	public void run() {
-		System.out.println("even task...Thread started with Name is: " + Thread.currentThread().getName());
-		
-		for(int i = 2; i< max; i+=2) {
+		for (int i = 2; i < max; i += 2) {
 			numberPrinter.printEven(i);
 		}
-			
+
 	}
 }
